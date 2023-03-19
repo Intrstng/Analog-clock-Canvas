67f30a9809888bd742d00ba76c8159872b79ca2d
Intrstng <babich_andrey@rambler.ru>
2023-03-19 22:48:31 +0300

    feat: add digital clock

--- scripts.js
+++ scripts.js
@@ -1,10 +1,12 @@
 let canvas;
 let radius;
-let now = new Date();
-let second = now.getSeconds();
-let minute = now.getMinutes();
-let hour = now.getHours() % 12 || 12;
-
+let fontSize;
+let canvasPadding;
+let angle;
+const date = new Date();
+let hour = date.getHours();
+const min = date.getMinutes();
+const sec = date.getSeconds();
 
 window.onload = function() {
   canvas = document.getElementById('clock');
@@ -24,6 +26,8 @@ function adaptiveResizeCanvas() {
   canvas.width = screenWidth > screenHeight ? screenHeight : screenWidth;
   canvas.height = screenHeight > screenWidth ? screenWidth : screenHeight;
   radius = canvas.width > canvas.height ? canvas.height / 2 : canvas.width / 2;
+  fontSize = radius * 0.12 + 'px';
+  canvasPadding = radius * 0.08;
 }
 
 function drawClockCanvas() {
@@ -38,114 +42,129 @@ function drawClockCanvas() {
     ctx.strokeStyle = 'rgb(200, 207, 145)';
     ctx.lineWidth = radius * 0.09;
 
-    // const outerWatermelonRindWidth = ctx.lineWidth;
-
     // draw gradient in center of clockface
     const grad = ctx.createRadialGradient(xpos, ypos, 1, xpos, ypos, radius / 1.25);
     grad.addColorStop(0,'rgb(255, 255, 255)');
     grad.addColorStop(1,'rgb(238, 62, 61)');
     ctx.fillStyle = grad;
-
-    ctx.arc(xpos, ypos, radius - ctx.lineWidth / 2, 0, 2 * Math.PI);
+    ctx.arc(xpos, ypos, radius - ctx.lineWidth / 2 - canvasPadding, 0, 2 * Math.PI);
     ctx.fill();
     ctx.stroke();
+    ctx.closePath();
 
     // draw yellow watermelon rind
     ctx.beginPath();
     ctx.strokeStyle = 'rgb(74, 147, 77)';
     ctx.lineWidth = radius * 0.03;
-    ctx.arc(xpos, ypos, radius - ctx.lineWidth / 2, 0, 2 * Math.PI);
+    ctx.arc(xpos, ypos, radius - ctx.lineWidth / 2 - canvasPadding, 0, 2 * Math.PI);
     ctx.stroke();
+    ctx.closePath();
     console.log("1", xpos, ypos)
 
-
-
-   
-
-
     // draw clock face digits
-    function createClockFace() {
-                          let degrees = 150;
-                          ctx.beginPath();
+    function createClockFace() {             
       ctx.translate(canvas.width / 2, canvas.width / 2);
 
+      for(let i = 1; i <= 12; i++) {
+        // draw clock face digits
+        ctx.save();
+        ctx.beginPath();
+        angle = i * Math.PI / 6;
+        ctx.rotate(angle);
+        ctx.translate(0, -radius * 0.63);
+        ctx.font = `bold ${fontSize} Lobster`;
+        ctx.fillStyle = "rgb(0, 0, 0)";
+        ctx.textBaseline = "middle";
+        ctx.textAlign = "center";
+        ctx.rotate(-angle);
+        ctx.fillText(i, 0, 0);
+        ctx.rotate(angle);
+        ctx.translate(0, radius * 0.63);
+        ctx.rotate(-angle);
+        ctx.closePath();
+        ctx.restore();
+          // draw hour dashes
+          ctx.save();
+          ctx.beginPath();
+          ctx.strokeStyle = "rgb(0, 0, 0)";
+          ctx.fillStyle = "rgb(0, 0, 0)";
+          ctx.lineWidth =  radius * 0.025;
+          ctx.lineCap = "round";
+          ctx.rotate(angle);
+          ctx.moveTo(radius * 0.73, 0);
+          ctx.lineTo(radius * 0.78,0);
+          ctx.stroke();
+          ctx.closePath();
+          ctx.restore();
+      }
+        // draw minute dashes
+        for(let i = 1; i <= 60; i++) {
+            ctx.save();
+            let angle2 = i * Math.PI / 30;
+            ctx.beginPath();
+            ctx.strokeStyle = "rgb(0, 0, 0)";
+            ctx.fillStyle = "rgb(0, 0, 0)";
+            ctx.lineWidth = radius * 0.0065;
+            ctx.lineCap = "square";
+            ctx.rotate(angle2);
+            ctx.moveTo(radius * 0.745, 0);
+            ctx.lineTo(radius * 0.78,0);
+            ctx.stroke();
+            ctx.closePath();
+            ctx.restore();
+        }
+    }
 
-    // Style digits of clock face
-    let fontSize = radius * 0.12 + 'px';
-    let angle;
 
-    // Rotate and put number and rotate back
-                                                              
-
-    for(let i = 1; i <= 12; i++) {
-      ctx.save();
-      angle = i * Math.PI / 6;
-      ctx.rotate(angle);
-      ctx.translate(0, -radius * 0.70);
-      ctx.font = `bold ${fontSize} Lobster`;
-      ctx.fillStyle = "rgb(0, 0, 0)";
-      ctx.textBaseline = "middle";
-      ctx.textAlign = "center";
-
-      ctx.rotate(-angle);
-      ctx.fillText(i, 0, 0);
-      ctx.rotate(angle);
-      ctx.translate(0, radius*0.70);
-      ctx.rotate(-angle);
-      ctx.restore();
-
-
-
-      ctx.save();
+    function showTimeDigitalClock() {
+      let fontSizeDigitalClock = parseInt(fontSize) * 0.9 + 'px';
+      const time = String(hour).padStart(2, 0) + ':' + String(min).padStart(2, 0) + ':' + String(sec).padStart(2, 0);
       ctx.beginPath();
-ctx.strokeStyle = "black"; // Задаём цвет обводки чёрный
-ctx.fillStyle = "black"; // Задаём цвет заливки чёрный
-ctx.lineWidth = 8; // Задаём ширину линий
-ctx.lineCap = "round"; // определяем как будут выглядеть концы линий
-
-ctx.rotate(angle);
-ctx.moveTo(radius * 0.8, 0);
-  ctx.lineTo(radius * 0.85,0);
-  ctx.stroke(); // Нарисовали то, что ранее описали
-
-
-      ctx.restore();
-  }
-
-                                                          
-
-
+      ctx.fillStyle='rgb(255, 0, 0)';
+      ctx.font = `bold ${fontSizeDigitalClock} "Rubik Iso"`;
+      ctx.fillText(time, -radius * 0.25, -radius * 0.28);
+      ctx.closePath();
+      // digitalClock.textContent = time;
+      // setTimeout(() => {
+      //   showTimeDigitalClock();
+      // }, 1000);
+    }
 
 
 
 
 
     
-// ctx.beginPath()
-// ctx.strokeStyle = "black"; // Задаём цвет обводки чёрный
-// ctx.fillStyle = "black"; // Задаём цвет заливки чёрный
-// ctx.lineWidth = 8; // Задаём ширину линий
-// ctx.lineCap = "round"; // определяем как будут выглядеть концы линий
- 
-// ctx.save(); // Сохраняем в контекст 
+    function calcTime() {
+      hour = date.getHours() % 12 || 12;
+    }
 
 
 
 
-}
+
+
+
+
 }
 createClockFace()
+showTimeDigitalClock()
+calcTime()
+
+
+
+
 }
 
 
 
 
 
-function findCenterOfClockFace() {
-  const canvas = document.getElementById('clock');
-  clockFaceCenterX = canvas.getBoundingClientRect().left + canvas.width / 2;
-  clockFaceCenterY = canvas.getBoundingClientRect().top + canvas.height / 2;
-}
+// function findCenterOfClockFace() {
+//   const canvas = document.getElementById('clock');
+//   clockFaceCenterX = canvas.getBoundingClientRect().left + canvas.width / 2;
+//   clockFaceCenterY = canvas.getBoundingClientRect().top + canvas.height / 2;
+// }
 
 
 
